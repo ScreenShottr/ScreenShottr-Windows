2fedb18fbc187d44e4fd17d66b279f2627d028e4
WickedGamingUK <09jonesd@warneford.swindon.sch.uk>
2014-04-09 11:19:41 +0100

    General Updates

---
+++ stdafx.h
@@ -0,0 +1,55 @@
+// stdafx.h : �W���̃V�X�e�� �C���N���[�h �t�@�C���̃C���N���[�h �t�@�C���A�܂���
+// �Q�Ɖ񐔂������A�����܂�ύX����Ȃ��A�v���W�F�N�g��p�̃C���N���[�h �t�@�C��
+// ���L�q���܂��B
+//
+
+#pragma once
+
+// ���Ŏw�肳�ꂽ��`�̑O�ɑΏۃv���b�g�t�H�[�����w�肵�Ȃ���΂Ȃ�Ȃ��ꍇ�A�ȉ��̒�`��ύX���Ă��������B
+// �قȂ�v���b�g�t�H�[���ɑΉ�����l�Ɋւ���ŐV���ɂ��ẮAMSDN ���Q�Ƃ��Ă��������B
+#ifndef WINVER				// Windows XP �ȍ~�̃o�[�W�����ɌŗL�̋@�\�̎g�p�������܂��B
+#define WINVER 0x0501		// ����� Windows �̑��̃o�[�W������ɓK�؂Ȓl�ɕύX���Ă��������B
+#endif
+
+#ifndef _WIN32_WINNT		// Windows XP �ȍ~�̃o�[�W�����ɌŗL�̋@�\�̎g�p�������܂��B                   
+#define _WIN32_WINNT 0x0501	// ����� Windows �̑��̃o�[�W������ɓK�؂Ȓl�ɕύX���Ă��������B
+#endif						
+
+#ifndef _WIN32_WINDOWS		// Windows 98 �ȍ~�̃o�[�W�����ɌŗL�̋@�\�̎g�p�������܂��B
+#define _WIN32_WINDOWS 0x0410 // ����� Windows Me �܂��͂���ȍ~�̃o�[�W������ɓK�؂Ȓl�ɕύX���Ă��������B
+#endif
+
+#ifndef _WIN32_IE			// IE 6.0 �ȍ~�̃o�[�W�����ɌŗL�̋@�\�̎g�p�������܂��B
+#define _WIN32_IE 0x0600	// ����� IE. �̑��̃o�[�W������ɓK�؂Ȓl�ɕύX���Ă��������B
+#endif
+
+// #define WIN32_LEAN_AND_MEAN		// Windows �w�b�_�[����g�p����Ă��Ȃ����������O���܂��B
+// Windows �w�b�_�[ �t�@�C��:
+#include <windows.h>
+
+// C �����^�C�� �w�b�_�[ �t�@�C��
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+// TODO: �v���O�����ɕK�v�Ȓǉ��w�b�_�[�������ŎQ�Ƃ��Ă��������B
+#include <strsafe.h>
+#include <shlobj.h> 
+#include "shlwapi.h"
+#pragma comment (lib, "Shlwapi.lib")
+
+#include <gdiplus.h>
+#pragma comment (lib, "Gdiplus.lib")
+using namespace Gdiplus;
+
+#include "wininet.h" 
+#pragma comment(lib,"wininet.lib")
+
+#include <time.h>
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
